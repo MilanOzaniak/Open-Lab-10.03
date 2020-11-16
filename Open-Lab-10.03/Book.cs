b7eb478cab58c6bf3398f7ba18599ad4e6f7c368
Ozaniak <Ozaniak@PC>
2020-11-16 12:36:07 +0100

    Add project files.

---
+++ Open-Lab-10.03/Book.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Open_Lab_10._03
+{
+    public class Book
+    {
+        private string title;
+        private string author;
+        private string category;
+        private int pages;
+        private int releaseDate;
+
+        public string Title
+        {
+            get;
+            set;
+        }
+        public string Author
+        {
+            get;
+            set;
+        }
+        public string Category
+        {
+            get;
+            set;
+        }
+        public int Pages
+        {
+            get { return pages; }
+            set
+            {
+                if (value < 0)
+                {
+                    pages = 1;
+                }
+                else
+                {
+                    pages = value;
+                }
+            }
+        }
+        public int ReleaseDate
+        {
+            get { return releaseDate; }
+            set
+            {
+                if (value > 2021)
+                {
+                    releaseDate = -1;
+                }
+                else if (value < 1450)
+                {
+                    releaseDate = -1;
+                }
+                else
+                {
+                    releaseDate = value;
+                }
+            }
+        }
+        public string Print
+        {
+            get { return $" Title is: {Title}\n Author: {Author}\n Release date: {ReleaseDate}\n Category of the book: {Category}\n Number of pages: {Pages}"; }
+        }
+    }
+}
---
+++ Open-Lab-10.03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Open_Lab_10._03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            book.Title = "The Fellowship of the Ring";
+            book.Author = "J. R. R. Tolkien";
+            book.Category = "fantasy and adventure";
+            book.Pages = 300;
+            book.ReleaseDate = 1954;
+
+            Console.WriteLine(book.Print);
+        }
+    }
+}
+
